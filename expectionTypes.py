5e85ecbdfae1f11e6ad2fd6c35e73bab1e088afd
isaac338365 <338365@edu.mkcollege.ac.uk>
2024-03-25 10:18:39 +0000

    basic changes

--- expectionTypes.py
+++ expectionTypes.py
@@ -4,7 +4,8 @@ from http import HTTPStatus
 
 
 cannot_find_email_password = HTTPException(
-    HTTPStatus.NOT_FOUND, "cannot find email and/or password"
+    HTTPStatus.UNAUTHORIZED, 
+    detail="cannot find email and/or password"
 )
 Incorrect_email_password = HTTPException(
     status_code=HTTPStatus.UNAUTHORIZED,
